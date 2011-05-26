054f64463652ab12ccb45373a68342d3029c690b
Graham Derryberry <g.derryberry@gmail.com>
2011-05-26 12:04:37 -0400

    Setting up todo list for this branch.
    --HG--
    branch : archRev

---
+++ hzarTESTsourcing.R
@@ -0,0 +1,20 @@
+## Files involved:
+
+## Core code files
+## hzarClasses.R
+## +-> modelEquationPrototypeGrouping.R
+
+
+
+#### Stuff I am ignoring for now.
+
+## Extensions
+## hzarPlotting.R
+## hzarMultiModel.R
+
+## Test data
+## molecular-data-robb2.csv
+## RailGenData.csv
+
+## Logging / testing 
+## oldTests/
