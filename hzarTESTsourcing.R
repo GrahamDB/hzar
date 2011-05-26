2dcc00d7cda2967014108116af24712bf07238e4
Graham Derryberry <g.derryberry@gmail.com>
2011-05-26 12:36:46 -0400

    Documentation of proposed re-organization of hzarClasses.R, in preperation for initial split.
    --HG--
    branch : archRev

--- hzarTESTsourcing.R
+++ hzarTESTsourcing.R
@@ -5,6 +5,49 @@
 ## +-> modelEquationPrototypeGrouping.R
 
 
+## ignoring
+## plotClineFunc
+
+#### prep work functions
+
+## setup transect with molecular data.
+## sampleLikelihoodMolecularPop
+## doMolecularData1DPops
+
+## helper functions for model setup
+## mkParam
+## setupMoleCenterClineParameters
+## buildCline1D
+
+## model templates
+
+## model setup
+## makeSimpleCline1D
+## +->buildCline1D
+## makeTailedCline1D
+## +->buildCline1D
+## makeCline1D
+## +->buildCline1D
+
+#### fitting functions and their helpers
+
+## helper functions for fitCline
+## getCredibleLLspace
+## getCredibleCutG
+## getCredibleCut
+## cline.unscale
+## cline.logit
+## splitParameters
+## cssp
+## cline.samp.dist
+
+## model fitting
+## fitClineModel
+## +->cssp
+## +->splitParameters
+## reFitClineFunc
+## +->splitParameters
+
 
 #### Stuff I am ignoring for now.
 
