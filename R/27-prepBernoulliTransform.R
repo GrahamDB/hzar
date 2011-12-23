e3b3052a64da85397c28096507bbd3cff003c66f
Graham Derryberry <g.derryberry@gmail.com>
2011-12-23 15:47:49 -0600

    Release of hzar 0.1-6
    This release includes the updated method for choosing the cutValue for the
    bernoulli transform, as well as an updated method for clustering individual
    measurements into population samples.
    
    --HG--
    branch : pkg-rc

--- R/27-prepBernoulliTransform.R
+++ R/27-prepBernoulliTransform.R
@@ -1,9 +1,126 @@
 
-hzar.dBernoulli.LL <- function(values,locations,getMax=FALSE,getProbs=FALSE){
-  midpoints<-function(x) {
+
+## mkn.charLocTwinV
+tVec.make <- function(loc,locDist,char,charLoc){
+  keepVals=!is.na(char);
+  char=char[keepVals]
+  charLoc=charLoc[keepVals];
+##   print(loc);
+##   print(charLoc[1:10]);
+##   print(char[1:10]);
+  locF <- factor(loc,sort(unique(c(as.character(loc),
+                                   as.character(charLoc)))))
+   ##  mkn.fixFactor(loc,charLoc);         
+## print(locF);
+  charLocF <- factor(charLoc,levels(locF));
+  distOfLoc <- locDist;names(distOfLoc) <- locF;
+  distOfLoc[sapply(levels(locF), function(x) which(x==locF)[[1]])]->distOfLoc;
+  return(list(dLoc=distOfLoc,
+              lChar=data.frame(loc=charLocF,chData=char)));
+}
+
+mkn.fixFactor <- function(x,levelsOfX=levels(x)){
+  return(factor(x,sort(unique(c(as.character(x),
+                                as.character(levelsOfX))))));
+}
+
+bLLdist <- function(series,totals){
+  hS <- ifelse(series>0,series*log(totals/series),0);
+
+  return(sum(hS));
+}
+
+wMax<-function(x)which(x==max(x))
+
+
+
+bLLSumLocDistFreq <- function(tVec,cutValues,
+                    values=tVec$lChar$chData,
+                    locations=tVec$lChar$loc){
+  cV=sort(unique(cutValues));
+  pL=lapply(cV,function(x) locations[values<x]);
+  pL=c(pL,list(locations));
+  
+  cL=lapply(pL,summary);
+  sL=c(cL[1],lapply(2:length(cL),function(x)cL[[x]]-cL[[x-1]]))
+  mL=matrix(unlist(sL),ncol=length(cL),byrow=FALSE)
+  mP=mL/apply(mL,1,sum)
+  
+  return(sum(ifelse(mP>0,
+                    mP*log(rep(apply(mP,2,sum),
+                               each=nrow(mP)
+                               ) / mP
+                           ),
+                    0)) );
+  ## bLLdist(mP,rep(apply(mP,2,sum),each=nrow(mP)));
+}
+
+midpoints<-function(x) {
     y<-as.numeric(sort(unique(x)));
     return((y[y>min(y)]+y[y<max(y)])/2);
   }
+
+
+bLLSumDistSF <- function(tVec,
+                    values=tVec$lChar$chData,
+                    locations=tVec$lChar$loc){
+  mVal=midpoints(values);
+  data.frame(char=mVal,info=sapply(mVal,bLLSumLocDistFreq,tVec=tVec,values=values,locations=locations))
+}
+bLLSLDSFchoice <- function(tVec,
+                    values=tVec$lChar$chData,
+                    locations=tVec$lChar$loc){
+  mVal=midpoints(values);
+  return(mVal[which.min(sapply(mVal,bLLSumLocDistFreq,tVec=tVec,values=values,locations=locations))]);
+}
+
+
+pullMinScore <- function(bFrame) bFrame$char[wMax(-bFrame$info)]
+
+
+hzar.dBernoulli.LL <- function(values,locations,getMax=FALSE,getProbs=FALSE){
+  if(getProbs){
+    cV <- bLLSLDSFchoice(NULL,values=values,locations=locations);
+  pL<-lapply(unique(locations),function(x) as.numeric(values[locations==x]));
+    nSamples<-as.numeric(lapply(pL,length));
+    nLow<-as.numeric(lapply(pL,function(x,cutValue) sum(x<cutValue), cV) )
+    return(data.frame(locID=unique(locations),
+                      pObs=nLow/nSamples,
+                      nLow=nLow,
+                      nSamples=nSamples,
+                      cutValue=cV));
+    
+    
+  }
+  if(getMax){
+    return(bLLSLDSFchoice(NULL,values=values,locations=locations))
+  }
+  return(bLLSumDistSF(NULL,values=values,locations=locations)$info);
+} 
+tVec.dBernoulli.LL <- function(tVec,
+                               values=tVec$lChar$chData,
+                               locations=tVec$lChar$loc,
+                               getMax=FALSE,getProbs=FALSE){
+  if(getProbs){
+    cV <- bLLSLDSFchoice(tVec);
+    pL<-lapply(unique(locations),function(x) as.numeric(values[locations==x]));
+    nSamples<-as.numeric(lapply(pL,length));
+    nLow<-as.numeric(lapply(pL,function(x,cutValue) sum(x<cutValue), cV) )
+    return(data.frame(locID=unique(locations),
+                      pObs=nLow/nSamples,
+                      nLow=nLow,
+                      nSamples=nSamples,
+                      cutValue=cV));
+    
+    
+  }
+  if(getMax){
+    return(bLLSLDSFchoice(tVec));
+  }
+  return(bLLSumDistSF(tVec)$info);
+} 
+old.dBernoulli.LL <- function(values,locations,getMax=FALSE,getProbs=FALSE){
+  
   mVal<-midpoints(values);
   bernoulli.LL <- function(nSuccess,nTotal){
     nFailures<-nTotal-nSuccess;
@@ -45,7 +162,29 @@ hzar.makeTraitObsData <- function(distOfLocation,locationOfValue,values){
                                   nSamples=probs$nSamples));
 }
 
+tVec.makeTraitObsData <- function(tVec){
+  probs <- tVec.dBernoulli.LL(tVec,getProbs=TRUE)
+  return(hzar.doMolecularData1DPops(distance=tVec$dLoc[probs$locID],
+                                    pObs=probs$pObs,
+                                    nSamples=probs$nSamples));
+}
+
+
 hzar.doMorphoSets <- function(traitNames, tDist, tDLocCol, tDDistCol, tValues, tVLocCol){
+
+  res <- lapply(traitNames,
+                function(id) {
+                  return(tVec.makeTraitObsData(tVec.make(loc=tDist[[tDLocCol]],
+                                                                locDist=tDist[[tDDistCol]],
+                                                                char=tValues[[id]],
+                                                                charLoc=tValues[[tVLocCol]])));
+                  }
+                )
+  names(res) <- traitNames;
+  return(res);
+}
+
+old.doMorphoSets <- function(traitNames, tDist, tDLocCol, tDDistCol, tValues, tVLocCol){
   distOfLocation <- tDist[[tDDistCol]];
   names(distOfLocation) <- tDist[[tDLocCol]];
   distOfLocation[sapply(levels(tDist[[tDLocCol]]),
