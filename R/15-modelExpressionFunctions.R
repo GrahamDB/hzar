e49b3b8ddff497fc4d1709c6458aeb27f69586f0
Graham Derryberry <g.derryberry@gmail.com>
2013-04-22 11:11:45 -0400

    Added expression functions for ML muL and muR parameters
    --HG--
    branch : br0-2

--- R/15-modelExpressionFunctions.R
+++ R/15-modelExpressionFunctions.R
@@ -90,3 +90,36 @@ guassianThetaLLExpF <- function( sampleMean, sampleVar, nEff,distance, muExp, va
              list(sM=sampleMean, sV=sampleVar,nE=nEff,
                   mE=eval(substitute(substitute(a,list(x=distance)),list(a=muExp))),
                   vE=eval(substitute(substitute(a,list(x=distance)),list(a=varExp)))))
+
+thEvalFExp <- function( target, exp, val)
+  bquote(.(target) <- .(eval(substitute(substitute(a,b),
+                                        list(a=exp,b=val)))))
+optMuPExpF <- function( sampleMean, nEff,distance,
+                       fExp, varExp,
+                       tMuL=quote(muL),
+                       tMuR=quote(muR)){
+  fTa <- quote(fXA)
+  fTd <- quote(fXD)
+  varT <- quote(vX)
+  cLT <- quote(cL)
+  cRT <- quote(cR)
+  aLT <- quote(aL)
+  aRT <- quote(aR)
+  aMT <- quote(aM)
+  tDen <- quote(aLaRaMM)
+  
+  
+  c(thEvalFExp(fTa,fExp,list(x=distance)),
+    bquote(.(fTd) <- 1 - .(fTa)),
+    thEvalFExp(varT,varExp,list(x=distance)),
+    bquote(.(cLT) <- sum( .(sampleMean)*.(nEff)*.(fTa)/ .(varT))),
+    bquote(.(cRT) <- sum( .(sampleMean)*.(nEff)*.(fTd)/ .(varT))),
+    bquote(.(aLT) <- sum( .(nEff)*.(fTa)^2/ .(varT))),
+    bquote(.(aRT) <- sum( .(nEff)*.(fTd)^2/ .(varT))),
+    bquote(.(aMT) <- sum( .(nEff)*.(fTd)*.(fTa)/ .(varT))),
+    bquote(.(tDen) <- .(aRT)*.(aLT)-.(aMT)^2),
+    bquote(.(tMuL) <- (.(cLT)*.(aRT)-.(cRT)*.(aMT))/.(tDen)),
+    bquote(.(tMuR) <- (.(cRT)*.(aLT)-.(cLT)*.(aMT))/.(tDen))
+    )
+}
+
